f3471392108bb8d5db86643c9aee488fb2e0d627
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2024-05-03 17:52:14 +0200

    feat: added saw trap

--- Assets/Scripts/Enemy_Sideways.cs
+++ Assets/Scripts/Enemy_Sideways.cs
@@ -2,14 +2,48 @@ using UnityEngine;
 
 public class Enemy_Sideways : MonoBehaviour
 {
-
     [SerializeField] private int damage;
+    [SerializeField] private float moveDistance;
+    [SerializeField] private float moveSpeed;
+    private float startingXPos;
+    private bool movingRight;
+
+    private void Awake()
+    {
+        startingXPos = transform.position.x;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             collision.GetComponent<Health>().TakeDamage(damage);
+        }
+    }
+
+    private void Update()
+    {
+        if (movingRight)
+        {
+            if (transform.position.x < startingXPos + moveDistance)
+            {
+                transform.position = new Vector3(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+            } 
+            else 
+            {
+                movingRight = false;
+            }
+        } 
+        else
+        {
+            if (transform.position.x > startingXPos - moveDistance) 
+            {
+                transform.position = new Vector3(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+            } 
+            else
+            {
+                movingRight = true;
+            }
         }
     }
 }
