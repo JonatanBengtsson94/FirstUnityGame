0394ee2b14acc1fb06f1abfb2d24ebe29dcb2699
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2024-05-04 14:31:01 +0200

    feat: added health pickup

---
+++ Assets/Scripts/Health/HealthPickup.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private int healthValue;
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.GetComponent<Health>().TakeDamage(-50);
+            gameObject.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/Health/Healthbar.cs
+++ Assets/Scripts/Health/Healthbar.cs
@@ -6,11 +6,6 @@ public class Healthbar : MonoBehaviour
     [SerializeField] private Health playerHealth;
     [SerializeField] private Text healthText;
 
-    private void Start()
-    {
-        //playerHealth = GetComponent<Health>();
-        //healthText = GetComponent<Text>();
-    }
     void Update()
     {
         healthText.text = $"Health: {playerHealth.CurrentHealth}";
