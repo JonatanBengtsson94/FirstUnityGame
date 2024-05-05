68e540752d7c8a858faaf65384e1a496d0350095
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2024-05-05 18:26:26 +0200

    fix: refactored attack code

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -1,62 +1,45 @@
 using UnityEngine;
 
-public class PlayerAttack : MonoBehaviour
+public class PlayerAttack: MonoBehaviour
 {
-    private enum Weapon
-    {
-        Gun,
-        Bomb
-    }
-    [SerializeField] private Weapon equippedWeapon;
-    [SerializeField] private float attackCooldown = 1;
-    private float cooldownTimer = Mathf.Infinity;
-    [SerializeField] private Transform firePoint;
-    [SerializeField] private GameObject[] bullets;
-    [SerializeField] private GameObject bomb;
-    private Animator animator;
+    protected Animator animator;
+    [Header("Gun attack")]
+    [SerializeField] private float gunCooldown;
 
-    private void Awake()
+    [SerializeField] RangedAttack gunAttack;
+    [Header("Bomb attack")]
+    [SerializeField] RangedAttack bombAttack; 
+    [SerializeField] private float bombCooldown;
+    private float gunTimer, bombTimer; 
+
+    void Awake()
     {
-        animator = GetComponent<Animator>();        
+        animator = GetComponent<Animator>();
     }
 
-    private void Update()
+    void Update()
     {
-        if (Input.GetMouseButton(0) && cooldownTimer > attackCooldown)
+        gunTimer += Time.deltaTime;
+        bombTimer += Time.deltaTime;
+        if (Input.GetMouseButton(0) && gunTimer > gunCooldown)
         {
-            Attack(equippedWeapon);
+            FireGun();
+            gunTimer = 0;
         }
-        cooldownTimer += Time.deltaTime;
-    }
-
-    private void Attack(Weapon weapon)
-    {
-        animator.SetTrigger("attack");
-        cooldownTimer = 0;
-
-        switch (weapon)
+        if (Input.GetMouseButton(1) && bombTimer > bombCooldown)
         {
-            case Weapon.Gun:
-                GetBullet().transform.position = firePoint.position;
-                GetBullet().GetComponent<Projectile>().SetDirection(Mathf.Sign(transform.localScale.x), 0);
-                break;
-            case Weapon.Bomb:
-                bomb.transform.position = firePoint.position;
-                bomb.GetComponent<Projectile>().SetDirection(Mathf.Sign(transform.localScale.x), 1);
-                break;
- 
-        } 
-    }
-
-    private GameObject GetBullet()
-    {
-        foreach (var bullet in bullets)
-        {
-            if (!bullet.activeInHierarchy)
-            {
-                return bullet;
-            }
+            ThrowBomb();
+            bombTimer = 0;
         }
-        return null;
+    }
+
+    private void ThrowBomb()
+    {
+        bombAttack.Attack(1);
+    }
+
+    private void FireGun()
+    {
+        gunAttack.Attack(0);
     }
 }
