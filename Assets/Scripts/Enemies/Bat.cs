b1b53f3ed670721d45f728040bbbe4d68711d778
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2024-05-20 18:10:20 +0200

    feat: made bat enemy charge player when in range

--- Assets/Scripts/Enemies/Bat.cs
+++ Assets/Scripts/Enemies/Bat.cs
@@ -3,15 +3,35 @@ using UnityEngine;
 public class Bat : MonoBehaviour
 {
   private Vision vision;
-  private ChargeMovement movement;
+  private EnemyMovement movement;
+  [SerializeField] private BoxCollider2D boxCollider;
 
     void Awake()
     {
-        
+      boxCollider = GetComponent<BoxCollider2D>();
+      movement = GetComponentInParent<EnemyMovement>();
+      vision = GetComponent<Vision>();
     }
 
     void Update()
     {
+      if (vision.PlayerInSight(boxCollider, 10))
+      {
+        movement.enabled = true;
+      } 
+      else
+      {
+        movement.enabled = false;
+      }
         
+    }
+
+    private void OnDrawGizmos()
+    {
+      Gizmos.color = Color.red;
+      Gizmos.DrawWireCube(
+        boxCollider.bounds.center + new Vector3(5 / 2 * (transform.localScale.x), 0, 0),
+        new Vector3(boxCollider.bounds.size.x - 5, boxCollider.bounds.size.y + 10, boxCollider.bounds.size.z)
+      );
     }
 }
--- Assets/Scripts/Enemies/Charge.cs
+++ Assets/Scripts/Enemies/Charge.cs
@@ -2,5 +2,12 @@ using UnityEngine;
 
 public class Charge: EnemyMovement 
 {
-  
+  [SerializeField] private Transform player;  
+  private Vector3 targetPos = Vector3.zero;
+
+  void Update()
+  {
+    targetPos = new Vector3(player.position.x, player.position.y, transform.position.z);
+    transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+  }
 }
