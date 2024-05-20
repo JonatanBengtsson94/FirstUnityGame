e03862a54c763a5bd1ddd227a4b68bb6e1773644
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2024-05-20 18:08:59 +0200

    fix: added parameter to give vision more options

--- Assets/Scripts/Enemies/Vision.cs
+++ Assets/Scripts/Enemies/Vision.cs
@@ -5,11 +5,11 @@ public class Vision : MonoBehaviour
     [SerializeField] private LayerMask playerLayer;
     [SerializeField] private float visionRange;
 
-    public bool PlayerInSight(BoxCollider2D boxCollider)
+    public bool PlayerInSight(BoxCollider2D boxCollider, float addedY)
     {
         RaycastHit2D hit = Physics2D.BoxCast(
             boxCollider.bounds.center + new Vector3(visionRange/ 2 * transform.localScale.x, 0, 0),
-            new Vector3(boxCollider.bounds.size.x + visionRange, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
+            new Vector3(boxCollider.bounds.size.x + visionRange, boxCollider.bounds.size.y + addedY, boxCollider.bounds.size.z),
             0,
             Vector2.left,
             0,
